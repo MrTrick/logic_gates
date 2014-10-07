3edf70c3c817f10d6cc95671cbab3627ba085183
MrTrick <MrTrick@github>
2014-10-07 15:15:11 +1100

    Initial commit

---
+++ bar.js
@@ -0,0 +1,94 @@
+/**
+ * Define a bar - having a fixed length, an origin point and an angle.
+ *
+ * Includes useful geometric tools for constructing linkages
+ */
+var Bar = function(options) {
+   options = options || {};
+   this.origin = options.origin || {x:0,y:0}
+   this.length = options.length || 100;
+   this.angle = options.angle || 0;
+   this.fill = options.fill || [255,255,255];
+   this.stroke = options.stroke || [255,0,0];
+   this.thickness = options.thickness || 15;
+   
+   /**
+    * Get the end coordinates of the bar
+    */
+   this.end = function() {
+     return {
+       x: this.origin.x + Math.cos(this.angle) * this.length, 
+       y: this.origin.y + Math.sin(this.angle) * this.length
+     };
+   }.bind(this);
+     
+   /**
+    * Given another bar, find an angle for both bars that allows the ends to be joined.
+    * Note that this does not permanently imply a join - joinEnds needs to be called each time the bars are moved.
+    */
+   this.joinEnds = function(bar, anti) {
+     //How far apart are the origins?
+     var d = Math.sqrt( Math.pow(bar.origin.x - this.origin.x, 2) + Math.pow(bar.origin.y - this.origin.y, 2) );
+     var d_angle = Math.atan2( bar.origin.y - this.origin.y ,  bar.origin.x - this.origin.x );
+               
+     //a2 = b2 + c2 - 2bc cos A ... A = acos( (b2 + c2 - a2) / 2bc )
+     var a_angle = Math.acos( (this.length*this.length + d*d - bar.length*bar.length) / (2*this.length*d) );
+     var b_angle = Math.acos( (bar.length*bar.length + d*d - this.length*this.length) / (2*bar.length*d) );
+       
+     if (anti) {
+       this.angle = d_angle + a_angle;
+       bar.angle = Math.PI + d_angle - b_angle;
+     } else {
+       this.angle = d_angle - a_angle;
+       bar.angle = Math.PI + d_angle + b_angle;
+     }
+   }.bind(this);
+     
+   /**
+    * Given a Y value, find an angle for the bar that puts the end at that point.
+    * Useful for simulating slots etc
+    */
+   this.setEndY = function(y, anti) {
+     this.angle = Math.asin( (y - this.origin.y) / this.length );
+     if (anti) this.angle = Math.PI - this.angle;
+   }.bind(this);
+     
+   /**
+    * Given an X value, find an angle for the bar that puts the end at that point.
+    * Useful for simulating slots etc
+    */
+   this.setEndX = function(x, anti) {
+     this.angle = Math.acos( (x - this.origin.x) / this.length );
+     if (anti) this.angle = Math.PI - this.angle;
+   }.bind(this);
+          
+   /**
+    * Draw the bar on the Processing screen.
+    */
+   this.draw = function(processing) {
+     processing.fill.apply(processing, this.fill);
+     processing.stroke.apply(processing, this.stroke);
+     var end = this.end();
+     var cos45 = Math.cos(this.angle + Math.PI/4), 
+         sin45 = Math.sin(this.angle + Math.PI/4);
+     processing.quad(
+       this.origin.x - cos45 * this.thickness,
+       this.origin.y - sin45 * this.thickness,
+       this.origin.x - sin45 * this.thickness,
+       this.origin.y + cos45 * this.thickness,
+       end.x + cos45 * this.thickness,
+       end.y + sin45 * this.thickness,
+       end.x + sin45 * this.thickness,
+       end.y - cos45 * this.thickness
+     );
+     processing.line(
+       this.origin.x, 
+       this.origin.y, 
+       end.x,
+       end.y
+     );
+   }.bind(this);
+};
+
+
+
---
+++ or.js
@@ -0,0 +1,91 @@
+var canvas = document.getElementById("canvas");
+var p = new Processing(canvas, function(processing) {
+  processing.height = 500;
+  processing.width = 500;
+  var font = processing.loadFont("Courier New");
+  processing.textFont(font, 24);
+
+  //Configuration:
+  //----------------
+  
+  // 12345678
+  // /```\___
+  // __/```\_
+  function a_x(t) { t = t % 8; return t<1 ? 0.5-0.5*Math.cos(Math.PI*t) : t<4 ? 1 : t<5 ? 0.5+0.5*Math.cos(Math.PI*t) : 0; }
+  function b_x(t) { t = t % 8; return t<2 ? 0 : t<3 ? 0.5-0.5*Math.cos(Math.PI*t) : t<6 ? 1 : t<7 ? 0.5+0.5*Math.cos(Math.PI*t) : 0; }
+  
+  var center = 120;  
+  var input_spacing = 100;
+  var in_link_length = 80;
+  var out_link_length = 28;
+  var stroke = 45;
+      
+  /*var in_link_length = 54.85;
+  var out_link_length = 20;
+  var stroke = 45;*/
+
+
+  var a = new Bar({ origin:{x:100,y:center - input_spacing/2}, length:100, angle:0, stroke:[255,0,0]});
+  var b = new Bar({ origin:{x:100,y:center + input_spacing/2}, length:100, angle:0, stroke:[150,0,0]});
+  var a_link = new Bar({ length:in_link_length, stroke:[0,255,0]});
+  var b_link = new Bar({ length:in_link_length, stroke:[0,255,0]});
+  var o_link = new Bar({ length:out_link_length, stroke:[0,150,150]});
+  var o = new Bar({ length:200, stroke:[0,0,255]});
+  
+  var t=0, outputs=[];
+  processing.draw = function() {
+    t += 0.01;
+    if (t>=8) {
+      t = 0;
+      outputs=[];
+    }
+    
+    //Move the input beams
+    a.origin.x = 50+stroke*a_x(t);
+    b.origin.x = 50+stroke*b_x(t);
+    
+    //Move the input links
+    a_link.origin = a.end();
+    b_link.origin = b.end();
+    a_link.joinEnds(b_link, true);
+    
+    //Move the output link
+    o_link.origin = a_link.end();
+    o_link.setEndY(center, true);
+    o.origin = o_link.end();
+
+    //Drawing
+    processing.background(224);
+    a.draw(processing);
+    b.draw(processing);    
+    a_link.draw(processing);
+    b_link.draw(processing);
+    o_link.draw(processing);
+    o.draw(processing);
+    
+    //Record the output position
+    outputs.push(o.origin.x);
+    
+    //Graphing
+    processing.stroke(0,0,0);
+    processing.line(50,200,50,351);
+    processing.line(50,351,450,351);
+    processing.stroke(255,0,0);
+    for (var i=0;i<t;i+=0.01) processing.point(50 + i*50, 350 - stroke*a_x(i));
+    processing.stroke(150,0,0);
+    for (var i=0;i<t;i+=0.01) processing.point(50 + i*50, 350 - stroke*b_x(i));
+    processing.stroke(0,0,255);
+    for (var n=0;n<outputs.length;n++) processing.point(50 + n/2, 350 - outputs[n] + outputs[0]);
+    
+    //Instant values
+    processing.fill(0,0,0);
+    processing.text("OR GATE Simulation", 40,30);
+    processing.fill(255,0,0);
+    processing.text("A: " + Math.round( stroke*a_x(t) ), 40, 375);
+    processing.fill(150,0,0);
+    processing.text("B: " + Math.round( stroke*b_x(t) ), 40, 400);
+    processing.fill(0,0,255);
+    processing.text("O: " + Math.round( o.origin.x - outputs[0] ), 40, 425);
+  };
+});
+
